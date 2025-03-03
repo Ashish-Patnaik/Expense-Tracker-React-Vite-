65a8ebd60df238f19d20ea01a4032fa6294b0935
soumik7063 <ghataksoumik7063@gmail.com>
2025-03-04 00:14:31 +0530

    loading animation added

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className='loader'>
+        <img src="./infinite-spinner.svg" alt="" />
+    </div>
+  )
+}
+
+export default Loader
